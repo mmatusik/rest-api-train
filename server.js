a9692b2f61b1c0916eb082519186a0fd8f72b672
Michał <mmatusikpl@gmail.com>
2018-02-04 23:35:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var app = require('./app');
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+mongoURLLabel = "";
+
 
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
